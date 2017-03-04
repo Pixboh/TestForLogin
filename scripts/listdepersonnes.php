fb3ea5008c76e066f1f0cfa457998fccad22f67b
Pixboh <bohpape96@gmail.com>
2017-03-04 23:22:19 +0000

    Update listdepersonnes.php

--- scripts/listdepersonnes.php
+++ scripts/listdepersonnes.php
@@ -1,5 +1,5 @@
 <?php
-require('connexionbd.php');
+require('https://github.com/Pixboh/TestForLogin/edit/master/scripts/connexionbd.php');
 $requete = $bdd->query('SELECT * FROM PERSONNES ');
 $json=array();
 while($resultat = $requete->fetchAll(PDO::FETCH_ASSOC)){
